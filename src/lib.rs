3f3bb84253fbf056c47826d24535766357ec00f4
ngergs <niklas@ngergs.de>
2025-05-04 21:33:08 +0200

    feat: working output (but needs refactoring)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 use crate::crossplane::function_runner_service_server::FunctionRunnerService;
-use crate::crossplane::{ResponseMeta, RunFunctionRequest, RunFunctionResponse};
-use prost_types::Duration;
+use crate::crossplane::{Resource, ResponseMeta, RunFunctionRequest, RunFunctionResponse};
+use prost_types::value::Kind::{StringValue, StructValue};
+use prost_types::{Duration, Struct, Value};
+use std::collections::BTreeMap;
 use tonic::{Request, Response, Status};
 
 pub mod crossplane {
     include!("apiextensions.r#fn.proto.v1.rs");
 }
 
-pub mod example_crd {
+pub mod composite_resource {
     // todo: mapping are missing to get the inflight request to this type...
-    typify::import_types!(schema = "example/xr.jsonschema", derives = []);
+    typify::import_types!(schema = "example/xr.jsonschema");
+}
+
+pub mod output {
+    typify::import_types!(schema = "example/bucket.jsonschema");
 }
 
 pub struct ExampleFunction {}
@@ -23,6 +29,28 @@ impl FunctionRunnerService for ExampleFunction {
         println!("RunFunction request: {:?}", request);
         let request = request.into_inner();
 
+        let mut desired = request.desired.unwrap_or_default(); // MUST pass through any desired state we do not care about
+        desired.resources.insert(
+            "test-bucket".to_owned(),
+            Resource {
+                resource: Some(Struct {
+                    fields: BTreeMap::from([(
+                        "metadata".to_owned(),
+                        Value {
+                            kind: Some(StructValue(Struct {
+                                fields: BTreeMap::from([(
+                                    "name".to_owned(),
+                                    Value {
+                                        kind: Some(StringValue("test".to_owned())),
+                                    },
+                                )]),
+                            })),
+                        },
+                    )]),
+                }),
+                ..Default::default()
+            },
+        );
         let result = RunFunctionResponse {
             meta: request.meta.and_then(|meta| {
                 Some(ResponseMeta {
@@ -33,7 +61,7 @@ impl FunctionRunnerService for ExampleFunction {
                     }), // time the result can be cached. SHOULD be set.
                 })
             }),
-            desired: request.desired, // MUST pass through any desired state we do not care about
+            desired: Some(desired),
             context: request.context,
             results: vec![],
             requirements: None,
