88cba86ea12eaa2254433c86a25e87bcc30abc2b
ngergs <niklas@ngergs.de>
2025-05-05 21:45:54 +0200

    feat: working input usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use crate::composite_resource::XBuckets;
 use crate::crossplane::function_runner_service_server::FunctionRunnerService;
-use crate::crossplane::{Resource, ResponseMeta, RunFunctionRequest, RunFunctionResponse};
+use crate::crossplane::{ResponseMeta, RunFunctionRequest, RunFunctionResponse};
 use crate::output::S3BucketCrossplaneApiVersion::S3AwsUpboundIoV1beta1;
 use crate::output::S3BucketCrossplaneKind::Bucket;
 use crate::output::{
-    S3BucketCrossplane, S3BucketCrossplaneMetadata, S3BucketCrossplaneSpec,
-    S3BucketCrossplaneSpecForProvider,
+    S3BucketCrossplane, S3BucketCrossplaneMetadata, S3BucketCrossplaneMetadataAnnotations,
+    S3BucketCrossplaneSpec, S3BucketCrossplaneSpecForProvider,
 };
 use prost_types::Duration;
+use std::io::{Error, ErrorKind};
 use tonic::{Request, Response, Status};
 
 pub mod crossplane {
@@ -14,12 +16,45 @@ pub mod crossplane {
 }
 
 pub mod composite_resource {
-    // todo: mapping are missing to get the inflight request to this type...
+    use crate::crossplane::Resource;
+    use std::io::{Error, ErrorKind};
+
     typify::import_types!(schema = "example/xr.jsonschema");
+
+    impl TryFrom<Option<Resource>> for XBuckets {
+        type Error = Error;
+
+        fn try_from(value: Option<Resource>) -> Result<Self, Self::Error> {
+            let value =
+                value.ok_or(Error::new(ErrorKind::InvalidData, "resource field not set"))?;
+            let value = serde_json::to_value(
+                &value
+                    .resource
+                    .ok_or(Error::new(ErrorKind::InvalidData, "resource field not set"))?,
+            )?;
+            Ok(serde_json::from_value(value)?)
+        }
+    }
 }
 
 pub mod output {
+    use crate::crossplane::Resource;
+    use std::io::Error;
+
     typify::import_types!(schema = "example/bucket.jsonschema");
+
+    impl TryFrom<S3BucketCrossplane> for Resource {
+        type Error = Error;
+
+        fn try_from(value: S3BucketCrossplane) -> Result<Self, Self::Error> {
+            let value = serde_json::to_value(&value)?;
+            let fields = serde_json::from_value(value)?;
+            Ok(Resource {
+                resource: Some(fields),
+                ..Default::default()
+            })
+        }
+    }
 }
 
 pub struct ExampleFunction {}
@@ -32,31 +67,33 @@ impl FunctionRunnerService for ExampleFunction {
     ) -> Result<Response<RunFunctionResponse>, Status> {
         println!("RunFunction request: {:?}", request);
         let request = request.into_inner();
+        let xbucket: XBuckets = request
+            .observed
+            .ok_or(Error::new(
+                ErrorKind::InvalidData,
+                "composite resource field not set",
+            ))?
+            .composite
+            .try_into()?;
 
         let mut desired = request.desired.unwrap_or_default(); // MUST pass through any desired state we do not care about
-        let bucket = S3BucketCrossplane {
-            api_version: S3AwsUpboundIoV1beta1,
-            kind: Bucket,
-            metadata: S3BucketCrossplaneMetadata {
-                generate_name: "test".to_string(),
-                annotations: Default::default(),
-                labels: Default::default(),
-            },
-            spec: S3BucketCrossplaneSpec {
-                for_provider: S3BucketCrossplaneSpecForProvider {
-                    region: "test".to_string(),
+        for name in xbucket.spec.names.into_iter() {
+            let bucket = S3BucketCrossplane {
+                api_version: S3AwsUpboundIoV1beta1,
+                kind: Bucket,
+                metadata: S3BucketCrossplaneMetadata {
+                    annotations: S3BucketCrossplaneMetadataAnnotations {
+                        crossplane_io_external_name: Some(name.clone()),
+                    },
                 },
-            },
-        };
-        let val = serde_json::to_value(&bucket).map_err(|e| Status::internal(e.to_string()))?;
-        let fields = serde_json::from_value(val).map_err(|e| Status::internal(e.to_string()))?;
-        desired.resources.insert(
-            "test-bucket".to_owned(),
-            Resource {
-                resource: Some(fields),
-                ..Default::default()
-            },
-        );
+                spec: S3BucketCrossplaneSpec {
+                    for_provider: S3BucketCrossplaneSpecForProvider {
+                        region: xbucket.spec.region.clone(),
+                    },
+                },
+            };
+            desired.resources.insert(name, bucket.try_into()?);
+        }
         let result = RunFunctionResponse {
             meta: request.meta.and_then(|meta| {
                 Some(ResponseMeta {
