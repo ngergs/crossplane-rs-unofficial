71552da35b1d723a822cd33b4a0d7d9d9fc46c8f
ngergs <niklas@ngergs.de>
2025-06-10 20:30:28 +0200

    chore: build.rs comment

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_file = "schema/crossplane/apis/apiextensions/fn/proto/v1/run_function.proto";
     tonic_build::configure()
-        // ust prost_wkt_types to have serializable structs (used in src/lib.rs to map types)
+        // use prost_wkt_types to have serializable structs (used in ../example/src/lib.rs to map types)
         .extern_path(".google.protobuf.Struct", "::prost_wkt_types::Struct")
         .build_server(true)
         .out_dir("src")
