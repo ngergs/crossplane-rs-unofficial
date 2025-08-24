921a04d55e0494ee174ac76ba4cf6218ad70dcee
ngergs <niklas@ngergs.de>
2025-08-25 01:06:46 +0200

    feat: dependency updates (tonic/prost v0.14)

--- sdk/build.rs
+++ sdk/build.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let proto_file = "schema/crossplane/proto/fn/v1/run_function.proto";
     fs::create_dir_all("src/generated")?;
-    tonic_build::configure()
+    tonic_prost_build::configure()
         // use prost_wkt_types to have serializable structs (used in ../example/src/lib.rs to map types)
         .extern_path(".google.protobuf.Struct", "::prost_wkt_types::Struct")
         .build_server(true)
