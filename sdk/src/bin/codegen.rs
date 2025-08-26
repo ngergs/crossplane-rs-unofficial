69093d59212914b5755d81c986fa6b839b4bd0d4
ngergs <niklas@ngergs.de>
2025-08-26 22:16:17 +0200

    fix: document codegen

--- sdk/src/bin/codegen.rs
+++ sdk/src/bin/codegen.rs
@@ -15,51 +15,60 @@ use tempfile::{NamedTempFile, TempDir};
 #[cfg(feature = "codegen")]
 const TARGET_PATH: &str = "./src/generated/crossplane.rs";
 
-#[cfg(feature = "codegen")]
 #[tokio::main]
+/// Generates `src/generated/crossplane.rs` by fetching the Crossplane protobuf schema for the
+/// provided tag argument and generates Rust types from it.
+/// Errors if feature `codegen` is disabled (default).
+/// Usage example: `cargo run --features codegen codegen v2.0.2`
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tag = args().skip(2).exactly_one()?;
-    let mut proto_file = NamedTempFile::new()?;
-    let proto_url = &format!(
-        "https://raw.githubusercontent.com/crossplane/crossplane/refs/tags/{tag}/proto/fn/v1/run_function.proto"
-    );
-    let proto_rsp = reqwest::get(proto_url).await?;
-    if !proto_rsp.status().is_success() {
-        return Err(format!(
-            "Fetching crossplane protobuf file from {proto_url} failed with status code {}",
-            proto_rsp.status()
-        )
-        .into());
-    }
-    proto_file.write_all(proto_rsp.bytes().await?.as_ref())?;
+    #[cfg(not(feature = "codegen"))]
+    return Err("`codegen` feature must be enabled".into());
 
-    let target_temp_dir = TempDir::new()?;
-    std::fs::create_dir_all("src/generated")?;
-    tonic_prost_build::configure()
-        // use prost_wkt_types to have serializable structs (used in ../example/src/lib.rs to map types)
-        .extern_path(".google.protobuf.Struct", "::prost_wkt_types::Struct")
-        // we only need server, client is not in scope and transport is hanlded by tonic
-        .build_server(true)
-        .build_transport(false)
-        .build_client(false)
-        .out_dir(target_temp_dir.path())
-        .compile_protos(
-            &[proto_file.path()],
-            &[proto_file.path().parent().ok_or(
-                "temporary protobuf file is stored at a root location. Please report a bug.",
-            )?],
+    #[cfg(feature = "codegen")]
+    {
+        let tag = args().skip(2).exactly_one()?;
+        let mut proto_file = NamedTempFile::new()?;
+        let proto_url = &format!(
+            "https://raw.githubusercontent.com/crossplane/crossplane/refs/tags/{tag}/proto/fn/v1/run_function.proto"
+        );
+        let proto_rsp = reqwest::get(proto_url).await?;
+        if !proto_rsp.status().is_success() {
+            return Err(format!(
+                "Fetching crossplane protobuf file from {proto_url} failed with status code {}",
+                proto_rsp.status()
+            )
+            .into());
+        }
+        proto_file.write_all(proto_rsp.bytes().await?.as_ref())?;
+
+        let target_temp_dir = TempDir::new()?;
+        std::fs::create_dir_all("src/generated")?;
+        tonic_prost_build::configure()
+            // use prost_wkt_types to have serializable structs (used in ../example/src/lib.rs to map types)
+            .extern_path(".google.protobuf.Struct", "::prost_wkt_types::Struct")
+            // we only need server, client is not in scope and transport is hanlded by tonic
+            .build_server(true)
+            .build_transport(false)
+            .build_client(false)
+            .out_dir(target_temp_dir.path())
+            .compile_protos(
+                &[proto_file.path()],
+                &[proto_file.path().parent().ok_or(
+                    "temporary protobuf file is stored at a root location. Please report a bug.",
+                )?],
+            )?;
+
+        prepend_to_file(
+            target_temp_dir
+                .path()
+                .join("apiextensions.r#fn.proto.v1.rs"),
+            TARGET_PATH,
+            &format!(
+                "// Generated from Crossplane {tag} composite function protobuf schema: https://github.com/crossplane/crossplane/tree/{tag}/proto/fn/v1\n"
+            ),
         )?;
-
-    prepend_to_file(
-        target_temp_dir
-            .path()
-            .join("apiextensions.r#fn.proto.v1.rs"),
-        TARGET_PATH,
-        &format!(
-            "// Generated from Crossplane {tag} composite function protobuf schema: https://github.com/crossplane/crossplane/tree/{tag}/proto/fn/v1\n"
-        ),
-    )?;
-    Ok(())
+        Ok(())
+    }
 }
 
 #[cfg(feature = "codegen")]
@@ -81,8 +90,4 @@ fn prepend_to_file(
     file.write_all(prepend_data.as_bytes())?;
     file.write_all(&data)?;
     Ok(())
-}
-#[cfg(not(feature = "codegen"))]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Err("`codegen` feature must be enabled".into())
 }
